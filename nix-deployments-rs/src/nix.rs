e0d5f41f0ca2215d4a2388614455818a19719181
Dan <gilmour890@gmail.com>
2026-01-01 22:01:18 +0000

    Working on git operations and nix operations

--- nix-deployments-rs/src/nix.rs
+++ nix-deployments-rs/src/nix.rs
@@ -2,6 +2,22 @@ use crate::types::{AppError, Result};
 use std::process::Command;
 use serde_json;
 
+pub const BASE_REPO_PATH: &str = "/tmp/proxnix/repos"
+
 pub fn list_nix_configs(repo_path: &str) -> Result<Vec<String>> {
-    L
+    let nix_eval = Command::new("nix")
+        .arg("eval")
+        .arg(".#nixosConfigurations")
+        .arg("--apply")
+        .arg("builtins.attrNames")
+        .arg("--json")
+        .output()?;
+    if !nix_eval.status.success() {
+        return Err(AppError::CmdError(format!("Nix eval has failed with the exit code: {:?}", nix_eval.status.code())))
+    }
+    let stdout_bytes = nix_eval.stdout;
+    let output_string = String::from_utf8(stdout_bytes)?;
+
+    Ok(output_string)
+
 }
