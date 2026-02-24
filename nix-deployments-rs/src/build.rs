c222459d8066a35f75bf55a7da806e421a06a3cb
Dan <gilmour890@gmail.com>
2026-02-24 19:19:43 +0000

    build module mostly done, reconcile done

--- nix-deployments-rs/src/build.rs
+++ nix-deployments-rs/src/build.rs
@@ -4,7 +4,9 @@ use crate::qm::{
     qm_create, qm_destroy, qm_importdisk, qm_set_agent, qm_set_disk, qm_set_resources,
 };
 use crate::state::{full_diff, save_deployed_state, update_deployed_state_commit};
-use crate::types::{AppError, Result, StateDiff, UpdateAction, VMConfig, VMUpdate};
+use crate::types::{
+    AppError, RebuildStrategy, Result, StateDiff, UpdateAction, VMConfig, VMUpdate,
+};
 use std::collections::HashMap;
 
 pub fn provision_vm(config: &VMConfig, qcow2_path: &str) -> Result<()> {
@@ -29,4 +31,36 @@ pub fn build_all_configs(repo_url: &str, commit_hash: &str) -> Result<HashMap<St
         })
         .collect::<Result<HashMap<_, _>>>()?;
     Ok(builds)
+}
+
+pub fn reconcile(diff: StateDiff, built_configs: HashMap<String, String>) -> Result<()> {
+    for config in diff.to_create {
+        let qcow_path = built_configs
+            .get(&config.name)
+            .ok_or(AppError::CmdError("Could not create new VM".to_string()))?;
+        provision_vm(&config, qcow_path)?;
+    }
+    for vm in diff.to_delete {
+        qm_destroy(vm.vm_id)?;
+    }
+    for actions in diff.to_update {
+        match &actions.required_action {
+            UpdateAction::InPlace => {
+                qm_set_resources(actions.config.vm_id, &actions)?;
+            }
+            UpdateAction::Rebuild => {
+                let qcow_path = built_configs
+                    .get(&actions.config.name)
+                    .ok_or(AppError::CmdError("Could not create new VM".to_string()))?;
+                qm_destroy(actions.config.vm_id)?;
+                provision_vm(&actions.config, qcow_path)?;
+            }
+            UpdateAction::Protected => {
+                // TODO I need to log this properly
+                let message = format!("{} is protected! No action can be taken", actions.name);
+                println!("{}", message);
+            }
+        }
+    }
+    Ok(())
 }
