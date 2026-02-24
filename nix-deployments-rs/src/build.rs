779017132707ddce231ea29278c3c69d15709bc6
Dan <gilmour890@gmail.com>
2026-02-24 18:23:28 +0000

    Added the first few functions to the build module

--- nix-deployments-rs/src/build.rs
+++ nix-deployments-rs/src/build.rs
@@ -1,1 +1,32 @@
+use crate::git::git_ensure_commit;
+use crate::nix::{BASE_REPO_PATH, configure_dirs, list_nix_configs, nix_build};
+use crate::qm::{
+    qm_create, qm_destroy, qm_importdisk, qm_set_agent, qm_set_disk, qm_set_resources,
+};
+use crate::state::{full_diff, save_deployed_state, update_deployed_state_commit};
+use crate::types::{AppError, Result, StateDiff, UpdateAction, VMConfig, VMUpdate};
+use std::collections::HashMap;
 
+pub fn provision_vm(config: &VMConfig, qcow2_path: &str) -> Result<()> {
+    qm_create(config)?;
+    let disk_ref = qm_importdisk(config.vm_id, qcow2_path, &config.storage_location)?;
+    qm_set_disk(config.vm_id, &disk_ref, &config.disk_slot)?;
+    qm_set_agent(config.vm_id)?;
+
+    Ok(())
+}
+
+pub fn build_all_configs(repo_url: &str, commit_hash: &str) -> Result<HashMap<String, String>> {
+    let dest_path = format!("{}/{}", BASE_REPO_PATH, commit_hash);
+    git_ensure_commit(&repo_url, &dest_path, &commit_hash)?;
+    let config_names = list_nix_configs(&dest_path)?;
+    configure_dirs(&commit_hash, config_names.clone(), &dest_path)?;
+    let builds = config_names
+        .iter()
+        .map(|config_name| -> Result<(String, String)> {
+            let result_path = nix_build(config_name, &dest_path, commit_hash)?;
+            Ok((config_name.clone(), format!("{}/nixos.qcow2", result_path)))
+        })
+        .collect::<Result<HashMap<_, _>>>()?;
+    Ok(builds)
+}
--- nix-deployments-rs/src/nix.rs
+++ nix-deployments-rs/src/nix.rs
@@ -1,8 +1,8 @@
 use crate::types::{AppError, Result};
-use std::process::Command;
 use axum::serve::IncomingStream;
 use serde_json;
 use std::fs;
+use std::process::Command;
 
 pub const BASE_REPO_PATH: &str = "/tmp/proxnix/repos";
 
@@ -16,30 +16,41 @@ pub fn list_nix_configs(repo_path: &str) -> Result<Vec<String>> {
         .arg("--json")
         .output()?;
     if !nix_eval.status.success() {
-        return Err(AppError::CmdError(format!("Nix eval has failed with the exit code: {:?}", nix_eval.status.code())))
+        return Err(AppError::CmdError(format!(
+            "Nix eval has failed with the exit code: {:?}",
+            nix_eval.status.code()
+        )));
     }
     let stdout_bytes = nix_eval.stdout;
     let output_string = String::from_utf8(stdout_bytes)?;
-    let parsed: Vec<String> = serde_json::from_str(&output_string)?;   
+    let parsed: Vec<String> = serde_json::from_str(&output_string)?;
 
     Ok(parsed)
-
 }
 
 pub fn nix_build(config_name: &str, repo_path: &str, commit_hash: &str) -> Result<String> {
     let nix_build = Command::new("nix")
         .current_dir(repo_path)
         .arg("build")
-        .arg(format!(".#nixosConfigurations.{}.config.system.build.qcow2", config_name))
+        .arg(format!(
+            ".#nixosConfigurations.{}.config.system.build.qcow2",
+            config_name
+        ))
         .arg("--out-link")
-        .arg(format!("{}/{}/{}/result", repo_path, commit_hash, config_name))
+        .arg(format!(
+            "{}/{}/{}/result",
+            repo_path, commit_hash, config_name
+        ))
         .output()?;
     if !nix_build.status.success() {
-        return Err(AppError::CmdError(format!("Nix build has failed with the exit code: {:?}", nix_build.status.code())))
+        return Err(AppError::CmdError(format!(
+            "Nix build has failed with the exit code: {:?}",
+            nix_build.status.code()
+        )));
     }
     let result_path = format!("{}/{}/{}/result", repo_path, commit_hash, config_name);
-    
-    Ok(result_path)  
+
+    Ok(result_path)
 }
 
 // TODO I need to finish up some utils to initialise this dir on setup. I will probably do a utils module.
