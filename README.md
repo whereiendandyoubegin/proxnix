2e3219c09fffcef6f7dfcd625d303fc462762853
Dan <gilmour890@gmail.com>
2026-02-25 13:09:41 +0000

    Flake written

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# nix-deployments-rs
+# Proxnix
 
+Proxnix is a nix driven state controller written in rust for the proxmox platform. It follows GitOps principles similar to ArgoCD 
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "nix-deployments-rs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "nix-deployments-rs";
+        version = "0.1.0";
+        src = ./nix-deployments-rs;
+        cargoLock.lockFile = ./nix-deployments-rs/Cargo.lock;
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = [ pkgs.openssl pkgs.libgit2 ];
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pkgs.openssl pkgs.libgit2 pkgs.pkg-config pkgs.rustup ];
+      };
+    };
+}
