03623dbd10f75aa864ca59031bb4155fc247ac42
Dan <gilmour890@gmail.com>
2026-04-10 13:54:20 +0100

    Working on adding sozu to the installer

--- flake.nix
+++ flake.nix
@@ -17,8 +17,81 @@
         nativeBuildInputs = [ pkgs.pkg-config ];
         buildInputs = [ pkgs.openssl pkgs.libgit2 ];
       };
+      proxnixPkg = pkgs.rustPlatform.buildRustPackage commonAttrs;
+
+      sozuConfig = pkgs.writeText "sozu-config.toml" ''
+        command_socket = "/run/sozu/command.sock"
+        log_level      = "info"
+        log_target     = "stdout"
+        command_buffer_size     = 16384
+        max_command_buffer_size = 163840
+
+        [[listeners]]
+        protocol = "http"
+        address  = "0.0.0.0:80"
+      '';
+
+      sozuUnit = pkgs.writeText "sozu.service" ''
+        [Unit]
+        Description=Sozu reverse proxy
+        After=network.target
+
+        [Service]
+        Type=simple
+        ExecStart=${pkgs.sozu}/bin/sozu start --config /etc/sozu/config.toml
+        Restart=always
+        RestartSec=5
+        RuntimeDirectory=sozu
+        RuntimeDirectoryMode=0755
+
+        [Install]
+        WantedBy=multi-user.target
+      '';
+
+      proxnixUnit = pkgs.writeText "proxnix.service" ''
+        [Unit]
+        Description=Proxnix deployment pipeline
+        After=network.target sozu.service
+        Requires=sozu.service
+
+        [Service]
+        Type=simple
+        ExecStartPre=${proxnixPkg}/bin/proxnix --init
+        ExecStart=${proxnixPkg}/bin/proxnix
+        Restart=always
+        RestartSec=5
+
+        [Install]
+        WantedBy=multi-user.target
+      '';
+
+      installScript = pkgs.writeShellApplication {
+        name = "proxnix-install";
+        text = ''
+          echo "Installing proxnix and sozu..."
+
+          mkdir -p /etc/sozu
+          cp ${sozuConfig} /etc/sozu/config.toml
+          cp ${sozuUnit}   /etc/systemd/system/sozu.service
+          cp ${proxnixUnit} /etc/systemd/system/proxnix.service
+
+          systemctl daemon-reload
+          systemctl enable --now sozu
+          systemctl enable --now proxnix
+
+          echo "Done. sozu and proxnix are running."
+        '';
+      };
     in {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage commonAttrs;
+      packages.${system} = {
+        default = proxnixPkg;
+        sozu = pkgs.sozu;
+      };
+
+      apps.${system}.install = {
+        type = "app";
+        program = "${installScript}/bin/proxnix-install";
+      };
 
       checks.${system}.clippy = pkgs.rustPlatform.buildRustPackage (commonAttrs // {
         nativeBuildInputs = commonAttrs.nativeBuildInputs ++ [ pkgs.clippy ];
@@ -28,7 +101,7 @@
       });
 
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ pkgs.openssl pkgs.libgit2 pkgs.pkg-config pkgs.rustup ];
+        buildInputs = [ pkgs.openssl pkgs.libgit2 pkgs.pkg-config pkgs.rustup pkgs.sozu ];
       };
     };
 }
